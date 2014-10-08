f49f46f428dd253f8dda16aad00c57669eb6f609
Mikola Lysenko <mikolalysenko@gmail.com>
2014-10-08 15:40:12 -0500

    first runnable version

--- example.js
+++ example.js
@@ -2,7 +2,7 @@ var tape    = require('tape')
 var runTest = require('./run-test')
 
 function filter(caseName) {
-  return caseName > "textures_tex-sub-image-2d-bad-args"
+  return true
 }
 
 runTest({
